9bc31e4723cb11d06db865b3303e577f56d9d4bc
Taylor Morgan <taylorjmorgan@hotmail.com>
2022-06-14 16:17:14 -0600

    Initial Heroku deploy

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import Footer from './Footer';
 import Socials from './Socials';
 
 function Contact() {
+
+    const [buttonClicked, setButtonClicked] = useState('hidden');
+    function handleSubmit(event) {
+        event.preventDefault();
+    }
+
+    function handleClick(event) {
+        setButtonClicked('form-header');
+        event.preventDefault();
+    }
 
     return (
         <motion.div
@@ -22,11 +32,12 @@ function Contact() {
                         <div className='contact-form m-5'>
                             <input type='text' name='name' className='form-control my-2' placeholder='Your name' />
                             <input type='email' name='email' className='form-control my-2' placeholder='Your email' />
-                            <input type='text' name='_honey' style={{ display: 'none' }}></input>
-                            <input type='hidden' name='_captcha' value='false'></input>
-                            <input type='hidden' name='_next' value='www.localhost:3000/contact'></input>
+                            <input type='text' name='_honey' style={{ display: 'none' }} />
+                            <input type='hidden' name='_captcha' value='false' />
+                            <input type='hidden' name='_next' />
                             <textarea name='message' className='form-control my-2' placeholder='Your message' rows='4' />
-                            <button type='submit' className='btn-submit btn btn-md btn-outline-light my-2'>Submit</button>
+                            <button type='submit' onClick={handleClick} className='btn-submit btn btn-md btn-outline-light my-2'>Submit</button>
+                            <p className={buttonClicked}>Thanks for your submission!</p>
                         </div>
                     </form>
                 </div>
