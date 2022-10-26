f7076e7f510eccf8fdb5e230b1e0ebd8e22c7523
Taylor Morgan <100230329+TaylorJMorgan@users.noreply.github.com>
2022-10-26 13:40:38 -0600

    Refactor form, add input hook, remove unnecessary files

--- src/components/Contact/components/ContactForm.js
+++ src/components/Contact/components/ContactForm.js
@@ -1,107 +1,91 @@
-import React, { useEffect, useReducer, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import classes from './ContactForm.module.css';
 import { useForm } from '@formspree/react';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
-
-const emailReducer = (state, action) => {
-  if (action.type === 'USER_INPUT') {
-    return { value: action.val, isValid: action.val.includes('@') };
-  }
-  if (action.type === 'INPUT_BLUR') {
-    return { value: state.value, isValid: state.value.includes('@') };
-  }
-  return { value: '', isValid: false };
-};
-
-const nameReducer = (state, action) => {
-  if (action.type === 'USER_INPUT') {
-    return { value: action.val, isValid: action.val.trim().length > 5 };
-  }
-  if (action.type === 'INPUT_BLUR') {
-    return { value: state.value, isValid: state.value.trim().length > 5 };
-  }
-  return { value: '', isValid: false };
-};
-
-const messageReducer = (state, action) => {
-  if (action.type === 'USER_INPUT') {
-    return { value: action.val, isValid: action.val.trim().length > 20 };
-  }
-  if (action.type === 'INPUT_BLUR') {
-    return { value: state.value, isValid: state.value.trim().length > 20 };
-  }
-  return { value: '', isValid: false };
-};
+import useInput from '../../../hooks/use-input';
 
 const ContactForm = (props) => {
+  const [formSuccessfullySubmitted, setFormSuccessfullySubmitted] =
+    useState(false);
+
+  const {
+    value: enteredName,
+    isValid: enteredNameIsValid,
+    hasError: nameHasError,
+    valueChangeHandler: nameChangeHandler,
+    inputBlurHandler: nameBlurHandler,
+    reset: resetNameInput,
+  } = useInput((value) => value.trim() !== '');
+
+  const {
+    value: enteredEmail,
+    isValid: enteredEmailIsValid,
+    hasError: emailHasError,
+    valueChangeHandler: emailChangeHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: resetEmailInput,
+  } = useInput((value) => value.includes('@'));
+
+  const {
+    value: enteredMessage,
+    isValid: enteredMessageIsValid,
+    hasError: messageHasError,
+    valueChangeHandler: messageChangeHandler,
+    inputBlurHandler: messageBlurHandler,
+    reset: resetMessageInput,
+  } = useInput((value) => value.trim() !== '');
+
+  let formIsValid = false;
+
+  if (enteredNameIsValid && enteredEmailIsValid && enteredMessageIsValid) {
+    formIsValid = true;
+  }
+
   const [state, handleSubmit] = useForm('meqnddqv');
   const [validationText, setValidationText] = useState('');
-  const [formIsValid, setFormIsValid] = useState(false);
-
-  const [emailState, dispatchEmail] = useReducer(emailReducer, {
-    value: '',
-    isValid: null,
-  });
-
-  const [nameState, dispatchName] = useReducer(nameReducer, {
-    value: '',
-    isValid: null,
-  });
-
-  const [messageState, dispatchMessage] = useReducer(messageReducer, {
-    value: '',
-    isValid: null,
-  });
-
-  const { isValid: emailIsValid } = emailState;
-  const { isValid: nameIsValid } = nameState;
-  const { isValid: messageIsValid } = messageState;
-
-  const emailChangeHandler = (event) => {
-    dispatchEmail({ type: 'USER_INPUT', val: event.target.value });
-  };
-
-  const nameChangeHandler = (event) => {
-    dispatchName({ type: 'USER_INPUT', val: event.target.value });
-  };
-
-  const messageChangeHandler = (event) => {
-    dispatchMessage({ type: 'USER_INPUT', val: event.target.value });
-  };
-
-  const validateEmailHandler = () => {
-    dispatchEmail({ type: 'INPUT_BLUR' });
-  };
-
-  const validateNameHandler = () => {
-    dispatchName({ type: 'INPUT_BLUR' });
-  };
-
-  const validateMessageHandler = () => {
-    dispatchMessage({ type: 'INPUT_BLUR' });
-  };
-
-  useEffect(() => {
-    const identifier = setTimeout(() => {
-      console.log('Checking form validity');
-      console.log(state);
-      setFormIsValid(emailIsValid && nameIsValid && messageIsValid);
-    }, 500);
-
-    return () => {
-      clearTimeout(identifier);
-    };
-  }, [emailIsValid, nameIsValid, messageIsValid, state]);
 
   const submitHandler = (event) => {
     event.preventDefault();
+
+    if (!formIsValid) {
+      return;
+    }
+
     handleSubmit(event);
-    setValidationText(
-      "Thanks for your submission! I'll get back to you shortly."
-    );
+
+    setFormSuccessfullySubmitted(true);
+
+    resetNameInput();
+    resetEmailInput();
+    resetMessageInput();
+    console.log(state);
   };
+
+  const nameInputClasses = nameHasError
+    ? 'form-control is-invalid'
+    : 'form-control';
+
+  const emailInputClasses = emailHasError
+    ? 'form-control my-2 is-invalid'
+    : 'form-control my-2';
+
+  const messageInputClasses = messageHasError
+    ? 'form-control my-2 is-invalid'
+    : 'form-control my-2';
+
+  useEffect(() => {
+    if (nameHasError) {
+      setValidationText('Please enter a name.');
+    }
+    if (emailHasError) {
+      setValidationText('Please enter a valid email address.');
+    }
+    if (messageHasError) {
+      setValidationText('Please enter a message.');
+    }
+  }, [nameHasError, emailHasError, messageHasError]);
 
   return (
     <Container>
@@ -109,43 +93,57 @@ const ContactForm = (props) => {
         onSubmit={submitHandler}
         className={`mb-5 ${classes['contact-form']} w-auto`}
       >
-        <input
-          type='text'
-          name='name'
-          className='form-control my-2'
-          placeholder='Your name'
-          value={nameState.value}
-          onChange={nameChangeHandler}
-          onBlur={validateNameHandler}
-        />
-        <input
-          type='email'
-          name='email'
-          className='form-control my-2'
-          placeholder='Your email'
-          value={emailState.value}
-          onChange={emailChangeHandler}
-          onBlur={validateEmailHandler}
-        />
-        <textarea
-          name='message'
-          className='form-control my-2'
-          placeholder='Your message'
-          rows='4'
-          value={messageState.value}
-          onChange={messageChangeHandler}
-          onBlur={validateMessageHandler}
-        />
+        <div>
+          <input
+            id='name'
+            type='text'
+            name='name'
+            className={nameInputClasses}
+            placeholder='Your name'
+            value={enteredName}
+            onChange={nameChangeHandler}
+            onBlur={nameBlurHandler}
+          />
+        </div>
+        <div>
+          <input
+            id='email'
+            type='email'
+            name='email'
+            className={emailInputClasses}
+            placeholder='Your email'
+            value={enteredEmail}
+            onChange={emailChangeHandler}
+            onBlur={emailBlurHandler}
+          />
+        </div>
+        <div>
+          <textarea
+            id='message'
+            name='message'
+            className={messageInputClasses}
+            placeholder='Your message'
+            rows='4'
+            value={enteredMessage}
+            onChange={messageChangeHandler}
+            onBlur={messageBlurHandler}
+          />
+        </div>
         <Button
           type='submit'
           variant='outline-light'
           className={`${classes['btn-submit']} my-2`}
           disabled={!formIsValid}
         >
-          Submit
+          {nameHasError || emailHasError || messageHasError
+            ? validationText
+            : 'Submit'}
         </Button>
+
         <p className={classes['form-header']}>
-          {formIsValid ? validationText : ''}
+          {formSuccessfullySubmitted &&
+            'Thank you for your submission! I will get back to you shortly.'}
+          &#8203;{/* No-width space to ensure p tag occupies space */}
         </p>
       </form>
     </Container>
